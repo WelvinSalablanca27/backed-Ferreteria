2b6b26e9c8009e9075f0629cee2d408b39400896
Welvin Salablanca <stivensalablanca@gmail.com>
2025-09-09 08:44:17 -0600

    Mensaje

--- src/controllers/categorias.controller.js
+++ src/controllers/categorias.controller.js
@@ -12,3 +12,21 @@ export const obtenerCategorias = async (req, res) => {
     });
   }
 };
+
+// Obtener una categoría por su ID
+export const obtenerCategoria = async (req, res) => {
+try {
+const id_categoria = req.params.id_categoria;
+const [result] = await pool.query('SELECT * FROM categorias WHERE id_categoria= ?', [req.params.id_categoria])
+if (result.length <= 0) {
+return res.status(404).json({
+mensaje: `Error al leer los datos. ID ${id_categoria} no encontrado.`
+});
+}
+res.json(result[0]);
+} catch (error) {
+return res.status(500).json({
+mensaje: 'Ha ocurrido un error al leer los datos de las categorias.'
+});
+}
+};
--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { obtenerCategorias } from '../controllers/categorias.controller.js';
+import { obtenerCategorias, obtenerCategoria} from '../controllers/categorias.controller.js';
 
 const router = Router();
 
 router.get('/categorias', obtenerCategorias);
-
 // Rutas
+router.get('/categoria/:id_categoria', obtenerCategoria);
 export default router;
